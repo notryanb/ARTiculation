cd7f2be1bb9a34087749cbbbac1a21f6b2cd9988
notryanb <notryanb@gmail.com>
2015-03-20 17:00:49 -0400

    Initialized table and model for users,comments,photos,tags,photo_tags
    Seed four users.
    	new file:   app/models/comment.rb
    	new file:   app/models/photo.rb
    	new file:   app/models/photo_tag.rb
    	new file:   app/models/tag.rb
    	new file:   app/models/user.rb
    	new file:   db/migrate/20150320162707_create_users.rb
    	new file:   db/migrate/20150320163106_create_photos.rb
    	new file:   db/migrate/20150320164042_create_comments.rb
    	new file:   db/migrate/20150320164449_create_tags.rb
    	new file:   db/migrate/20150320164550_create_photo_tags.rb
    	modified:   db/seeds.rb

---
+++ db/migrate/20150320164042_create_comments.rb
@@ -0,0 +1,12 @@
+class CreateComments < ActiveRecord::Migration
+  def change
+    create_table :comments do |t|
+      t.string :title, null: false
+      t.text :content, null: false
+      t.belongs_to :user
+      t.belongs_to :photo
+
+      t.timestamps null: false
+    end
+  end
+end
---
+++ db/seeds.rb
@@ -0,0 +1,27 @@
+User.create(email:"kiran@gmail.com",
+            password:"123",
+            first_name: "Kiran",
+            last_name:"C",
+            handle: "kirawesome",
+            bio: "Awesome 24/7, yo!")
+
+User.create(email:"ryan@gmail.com",
+            password:"123",
+            first_name: "Ryan",
+            last_name:"B",
+            handle: "ryawesome",
+            bio: "Awesome 12/7, yo!")
+
+User.create(email:"angel@gmail.com",
+            password:"123",
+            first_name: "Angel",
+            last_name:"B",
+            handle: "awesomangel",
+            bio: "Whatever")
+
+User.create(email:"natalia@gmail.com",
+            password:"123",
+            first_name: "Natalia",
+            last_name:"B",
+            handle: "natawesome",
+            bio: "Awesome 24/7, yo!")
