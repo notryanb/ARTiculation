081cf57b8878e33ffe47778a312108f2d0ed726c
notryanb <notryanb@gmail.com>
2015-03-21 11:42:09 -0400

    Add non-functioning TAG dropdown form

--- app/controllers/index.rb
+++ app/controllers/index.rb
@@ -1,32 +1,33 @@
-def load_pictures
-  Dir.glob("public/img/*.{jpg,JPG}")
-end
+# def load_pictures
+#   Dir.glob("public/img/*.{jpg,JPG}")
+# end
 
 get '/' do
   erb :'index', locals: {pictures: load_pictures, tags: Tag.all}
 end
 
-get '/photos/:id/upload' do
-  erb :'photos/upload', locals: { user: User.find_by(id: params[:id]) }
-end
+# get '/photos/:id/upload' do
+#   erb :'photos/upload', locals: { user: User.find_by(id: params[:id]) }
+# end
 
-get '/photos/:id/new' do
-  user = User.find_by(id: params[:id])
-  erb :'photos/new', locals: {user: user}
-end
+# get '/photos/:id/new' do
+#   user = User.find_by(id: params[:id])
+#   all_tags = Tag.all
+#   erb :'photos/new', locals: {user: user, all_tags: all_tags}
+# end
 
-post '/photos/:id/new' do
-  @user = User.find_by(id: params[:id])
+# post '/photos/:id/new' do
+#   @user = User.find_by(id: params[:id])
 
-  # Write file to public/img directory for display
-  File.open('public/img/' + params['file'][:filename], "w") do |f|
-    f.write(params['file'][:tempfile].read)
-  end
+#   # Write file to public/img directory for display
+#   File.open('public/img/' + params['file'][:filename], "w") do |f|
+#     f.write(params['file'][:tempfile].read)
+#   end
 
-  @pic_to_add = File.open('public/img/' + params['file'][:filename])
-  Photo.create(url: @pic_to_add.path.sub!(/public\//, ''), user: @user)
+#   @pic_to_add = File.open('public/img/' + params['file'][:filename])
+#   Photo.create(url: @pic_to_add.path.sub!(/public\//, ''), user: @user)
 
-  erb :'photos/new', locals: {pictures: load_pictures}
-end
+#   erb :'photos/new', locals: {pictures: load_pictures}
+# end
 
 
--- app/controllers/photos.rb
+++ app/controllers/photos.rb
@@ -1,4 +1,4 @@
-get "/photos/:id" do
+get "/photos/show" do
   userr = User.find_by(id: session[:user_id])
   photo = Photo.find_by(id: params[:id])
   if photo
@@ -7,3 +7,37 @@ get "/photos/:id" do
     [402, "no photo with given id found"]
   end
 end
+
+
+#--------BEGIN RYAN'S CODE
+
+  # TODO: Move this method to photo model
+def load_pictures
+  Dir.glob("public/img/*.{jpg,JPG}")
+end
+
+get '/photos/:id/upload' do
+  erb :'photos/upload', locals: { user: User.find_by(id: params[:id]) }
+end
+
+get '/photos/:id/new' do
+  user = User.find_by(id: params[:id])
+  all_tags = Tag.all
+  erb :'photos/new', locals: {user: user, all_tags: all_tags}
+end
+
+post '/photos/:id/new' do
+  user = User.find_by(id: params[:id])
+  all_tags = Tag.all
+
+  # Write file to public/img directory for display
+  File.open('public/img/' + params['file'][:filename], "w") do |f|
+    f.write(params['file'][:tempfile].read)
+  end
+
+  @pic_to_add = File.open('public/img/' + params['file'][:filename])
+  Photo.create(url: @pic_to_add.path.sub!(/public\//, ''), user: user)
+
+  erb :'photos/show', locals: {pictures: load_pictures, user: user, all_tags: all_tags}
+end
+#----------END OF RYAN'S CODE
