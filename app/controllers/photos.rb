976a5d05e5206f017c34dccbecb8e5949e86f885
Notika314 <notika314@gmail.com>
2015-03-20 21:08:14 -0400

    edit

--- app/controllers/photos.rb
+++ app/controllers/photos.rb
@@ -1,7 +1,8 @@
 get "/photos/:id" do
+  userr = User.find_by(id: session[:user_id])
   photo = Photo.find_by(id: params[:id])
   if photo
-    erb(:"photos/show", locals: {picture: photo})
+    erb(:"photos/show", locals: {picture: photo, user: userr})
   else
     [402, "no photo with given id found"]
   end
